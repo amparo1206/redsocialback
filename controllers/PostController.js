76f0d47e59bc5c9418fffb60c1fa02e7d3437f6e
amparo1206 <amparo.frontend@gmail.com>
2022-02-01 13:42:12 +0100

    creando el post

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,5 +1,15 @@
 const Post = require("../models/Post");
 
 const PostController = {
-    async create (req)
+    async create(req, res) {
+        try {
+            const post = await Post.create({ ...req.body })
+            res.status(201).send(post)
+        } catch (error) {
+            console.error(error)
+            res.status(500).send({message:'we had a problem creating the post'})
+        }
+    }
 }
+
+module.exports = PostController;
