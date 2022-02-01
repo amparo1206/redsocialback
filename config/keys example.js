bdc728d3585b816b6ca8a8f1b80a7c2e93752fba
amparo1206 <amparo.frontend@gmail.com>
2022-02-01 13:30:44 +0100

    route de post

---
+++ config/keys example.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGO_URI: 'mongodb+ srv://<name>:<password>4@cluster0.m9xwg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
---
+++ controllers/PostController.js
@@ -0,0 +1,5 @@
+const Post = require("../models/Post");
+
+const PostController = {
+    async create (req)
+}
