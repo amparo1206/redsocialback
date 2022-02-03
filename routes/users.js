ec87de145dd826d434cd6b2c919b80a3d2ec99b6
amparo1206 <amparo.frontend@gmail.com>
2022-02-03 13:00:31 +0100

    logout users routes

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
 
-router.post('/register', UserController.register);
-router.post('/login', UserController.login);
+router.post('/register', UserController.register)
+router.post('/login', UserController.login)
+router.delete('/logout', UserController.logout)
 
 
 module.exports = router;
